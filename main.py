c875b73beb2c04b26f3158a742b4a435f92c88fa
allnc <meibrohimov@gmail.com>
2023-11-29 21:42:37 +0500

    🟢📡

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ def generators():
 
 def for_loop():
     temp_list: list[int] = []
-    
+    for x in range(10_000):
+        temp_list.append(x)
+
+    return temp_list
+
+
+if __name__ == '__main__':
+    ...
