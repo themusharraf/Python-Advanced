74798597af84feeccba3da25c873e708da1010c6
allnc <meibrohimov@gmail.com>
2023-11-30 13:48:49 +0500

    add decorator🟢

--- decorators.py
+++ decorators.py
@@ -3,3 +3,13 @@ def my_decorator(func):
         print("Funktsiya ishga tushdi! ")
         func()
         print("Funktsiya tuxtadi! ")
+
+    return wrapper
+
+
+@my_decorator
+def add_function(a: int, b: int, ) -> int:
+    return a + b
+
+
+print(add_function(3, 5))
