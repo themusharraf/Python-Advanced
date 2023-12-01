038fbc2398a88870f24f1bbe96f9b73abcfc559a
allnc <meibrohimov@gmail.com>
2023-12-01 14:09:52 +0500

    add decorator🟢

--- decorators.py
+++ decorators.py
@@ -1,14 +1,14 @@
-# def my_decorator(func):
-#     print("Funktsiya ishga tushdi! ")
-#     func()
-#     print("Funktsiya tuxtadi! ")
-#
-#
-# def my_func():
-#     print("hello def my function")
-#
-#
-# my_decorator(my_func)
+def my_decorator(func):
+    print("Funktsiya ishga tushdi! ")
+    func()
+    print("Funktsiya tuxtadi! ")
+
+
+def my_func():
+    print("hello def my function")
+
+
+my_decorator(my_func)
 # ________________________________________________________________
 # def my_decorator(func):
 #     def wrapper():
@@ -57,28 +57,28 @@
 #
 # print(divider(10, 5))  # 2.0
 # print(divider(10, 0))  # 10.0
-
-data_list = []
-
-
-def register_api(func):
-    data_list.append(func)
-    return func
-
-
-@register_api
-def get_api():
-    return "name-> Admin"
-
-
-def get_api1():
-    return "name-> User"
-
-
-def get_api2():
-    return "name-> username"
-
-
-if __name__ == "__main__":
-    for i in data_list:
-        print(i())
+#----------------------------------------------------------------
+# data_list = []
+#
+#
+# def register_api(func):
+#     data_list.append(func)
+#     return func
+#
+#
+# @register_api
+# def get_api():
+#     return "name-> Admin"
+#
+#
+# def get_api1():
+#     return "name-> User"
+#
+#
+# def get_api2():
+#     return "name-> username"
+#
+#
+# if __name__ == "__main__":
+#     for i in data_list:
+#         print(i())
