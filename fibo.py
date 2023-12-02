d897cb99119ff77fefdad61a6ba6f7aed52fd55f
allnc <meibrohimov@gmail.com>
2023-12-02 19:57:31 +0500

    add decorator🟢

--- fibo.py
+++ fibo.py
@@ -1,11 +1,20 @@
-def fib(n: int):
-    a: int = 0
-    b: int = 1
-    print(a)
-    print(b)
-    for x in range(2, n):
-        c = a + b
-        a = b
-        b = c
-        print(c)
-fib(1000)
+# def fib(n: int):
+#     a: int = 0
+#     b: int = 1
+#     print(a)
+#     print(b)
+#     for x in range(2, n):
+#         c = a + b
+#         a = b
+#         b = c
+#         print(c)
+# fib(1000)
+
+def fib(n):
+    if n in {0, 1}:  # Base case n == 1 or n == 2 return 1
+        return n
+    return fib(n - 1) + fib(n - 2)  # Recursive case
+
+
+for x in range(15):
+    print(fib(x))
